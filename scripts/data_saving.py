b3a6910cc178fb7d6a65ffec652cd96f9fe8662b
leuas <maybeisBenjamin@gmail.com>
2025-12-15 19:10:13 +0800

    add data preprocess function to data_saving.py

--- scripts/data_saving.py
+++ scripts/data_saving.py
@@ -2,7 +2,7 @@
 
 
 from src.utils.data_etl import get_and_save_liked_disliked_data_for_database,get_and_save_yt_video_for_database,get_and_save_his_data_for_database
-
+from src.utils.ops import like_dislike_streamlit_data_preprocess,interest_productive_data_preprocess
 
 
 if __name__ == '__main__':

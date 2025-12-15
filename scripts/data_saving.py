4920876d125644668005d133cac4ac92ea992517
leuas <maybeisBenjamin@gmail.com>
2025-12-15 19:08:57 +0800

    rename save_yt_data.py to data_saving.py

--- scripts/data_saving.py
+++ scripts/data_saving.py
@@ -1,4 +1,4 @@
-'''save your youtube data to database '''
+'''save your data to database '''
 
 
 from src.utils.data_etl import get_and_save_liked_disliked_data_for_database,get_and_save_yt_video_for_database,get_and_save_his_data_for_database

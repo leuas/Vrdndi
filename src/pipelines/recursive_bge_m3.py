d8d8de291fe9c7c09e734e981d364e681638f5f3
leuas <maybeisBenjamin@gmail.com>
2026-01-02 15:04:34 +0800

    remove redundant, improve clarity

--- src/pipelines/recursive_bge_m3.py
+++ src/pipelines/recursive_bge_m3.py
@@ -3,7 +3,11 @@
 import torch
 import torch.nn as nn
 import transformers
+import wandb
 
+from dataclasses import asdict
+from tqdm import tqdm
+from itertools import islice
 from typing import Iterator
 
 from transformers import DataCollatorWithPadding
@@ -25,6 +29,9 @@ class RecursiveBGETraining:
 
         self.config=config
 
+        if self.config.wandb_config.get('TrainingConfig') is None:
+            self.config.wandb_config['TrainingConfig']=asdict(self.config)
+
         self.ori_model=AutoModel.from_pretrained(self.config.ori_model_name).to(DEVICE)
 
         self.distill_model=DistillRecursiveModel(
@@ -38,34 +45,6 @@ class RecursiveBGETraining:
         
         self.data=RecursiveDataLoader(self.config)
 
-
-    def _sort_train_data_to_batch(self,data_stream:Iterator[str]):
-        '''process the data and sort them to batch '''
-
-        batch_texts = []
-        for _ in range(self.config.batch_size):
-            try:
-                # Grab next sentence from internet/dataset
-                sentence = next(data_stream) 
-                batch_texts.append(sentence)
-            except StopIteration:
-                break
-        
-        return batch_texts
-
-    def _tokenize_texts(self,batch_texts:list) ->transformers.tokenization_utils_base.BatchEncoding:
-        '''tokenize the text input '''
-        output = self.tokenizer(
-                    batch_texts, 
-                    padding=True, 
-                    truncation=True, 
-                    max_length=8192, 
-                    return_tensors="pt"
-                ).to(DEVICE)
-        
-        return output
-
-
     
     def _compute_loss(self,ori_output:torch.Tensor,distill_output:torch.Tensor,step_cost:torch.Tensor) ->torch.Tensor:
         '''compute the Embeddings loss between ori model and distill model'''
@@ -76,49 +55,131 @@ class RecursiveBGETraining:
         loss = similarity_loss + step_cost
 
         return loss
+    
+    def ori_model_predict(self,batch) ->torch.Tensor:
+        '''
+        use original model to predict current batch
+        '''
+
+        with torch.no_grad(): 
+            ori_model_output = self.ori_model(batch)
+            ori_model_state = ori_model_output.last_hidden_state
+            # Normalize Teacher (Crucial!)
+            ori_model_state_normed = torch.nn.functional.normalize(ori_model_state, dim=2)
+
+        return ori_model_state_normed
 
 
-    def train(self,save_name:str="bge_recursive.pth"):
+
+    def train(self,save_name:str="bge_recursive.pth") ->None:
         '''start training '''
 
         print("Starting Training Loop...")
         
-        data_stream=self.data.dataloader()
+        train_data=self.data.train_dataloader()
+        finite_train_data=islice(train_data,self.config.train_batch_num)
+        tqdm_data=tqdm(finite_train_data,desc="Runing Training Process")
 
         total_loss = 0
-    
-        batch_texts=self._sort_train_data_to_batch(data_stream)
 
         self.ori_model.eval()
         self.distill_model.train()
 
-        for batch in batch_texts:
+        for batch in tqdm_data:
 
-            with torch.no_grad(): 
-                ori_model_output = self.ori_model(batch)
-                ori_model_state = ori_model_output.last_hidden_state
-                # Normalize Teacher (Crucial!)
-                ori_model_state_normed = torch.nn.functional.normalize(ori_model_state, dim=2)
-            
+            ori_model_output=self.ori_model_predict(batch)
             
             distill_output, step_cost = self.distill_model.predict_step(batch)
 
-            loss=self._compute_loss(ori_model_state_normed,distill_output,step_cost)
-
+            loss=self._compute_loss(ori_model_output,distill_output,step_cost)
 
             # Backpropagation
-            self.optimizer.zero_grad() 
-            loss.backward()     
-            self.optimizer.step()     
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
             # Logging
             total_loss += loss.item()
 
+            wandb.log({"Training Loss":loss})
 
-    def eval(self):
+
+    def eval(self) -> None:
         '''
         Evaluate the model with some text in dataset
         '''
+
+        eval_data=self.data.eval_dataloader()
+
+        finite_eval_data=islice(eval_data,self.config.eval_batch_num)
+        tqdm_data=tqdm(finite_eval_data,desc="Evaluating Model Performance")
+        
+        self.ori_model.eval()
+        self.distill_model.eval()
+
+        total_loss=0
+
+        for batch in tqdm_data:
+
+            ori_model_output=self.ori_model_predict(batch)
+            
+            distill_output, step_cost = self.distill_model.predict_step(batch)
+
+            loss=self._compute_loss(ori_model_output,distill_output,step_cost)
+
+            total_loss+=loss.item()
+
+
+        avg_loss=total_loss/self.config.eval_batch_num
+
+        similarity_score = 1 - avg_loss
+    
+        print(f"Validation Loss: {avg_loss:.4f}")
+        print(f"Similarity: {similarity_score * 100:.2f}%")
+
+        wandb.log({"Validation Loss":avg_loss, "Similarity":similarity_score*100})
+
+
+
+    def epoch_training_loop(self):
+        '''train in a epoch loop'''
+
+        for _ in range(self.config.total_epoch):
+
+            self.train()
+
+            self.eval()
+
+
+    def start_train(self,run_name:str):
+        '''start the training process'''
+        wandb.init(
+            project='personal_feed',
+            name=run_name,
+            config=self.config.wandb_config
+            )
+
+        wandb.watch(self.distill_model,log='all',log_freq=10)
+
+        self.epoch_training_loop()
+
+        wandb.finish()
+
+        
+
+        
+
+        
+
+
+
+
+        
+
+        
+
+
+
 
 
         
